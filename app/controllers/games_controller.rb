c7b2668bf16122768b7dd5a5ecbccdc5cbd621d6
ujihisa <ujihisa@users.noreply.github.com>
2024-06-25 21:52:47 -0700

    No destroy as well

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: %i[ show edit update destroy ]
+  before_action :set_game, only: %i[ show ]
 
   # GET /games or /games.json
   def index
@@ -27,29 +27,6 @@ class GamesController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @game.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /games/1 or /games/1.json
-  def update
-    respond_to do |format|
-      if @game.update(game_params)
-        format.html { redirect_to game_url(@game), notice: "Game was successfully updated." }
-        format.json { render :show, status: :ok, location: @game }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /games/1 or /games/1.json
-  def destroy
-    @game.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to games_url, notice: "Game was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
