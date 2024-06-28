20d76124344aece94d07e80b120467e60f7cfc87
ujihisa <ujihisa@users.noreply.github.com>
2024-06-27 18:49:40 -0700

    Make the production system responds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  config.hosts = ['https://human-vs-pest-t7ero6olsa-uc.a.run.app/']
+  config.hosts = ['human-vs-pest-t7ero6olsa-uc.a.run.app']
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
